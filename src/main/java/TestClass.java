2a96340ab848bccd0bd6898fa93d48df7b0ba6ea
HP <alperengozeten@gmail.com>
2021-08-27 16:25:22 +0300

    Initial commit

---
+++ src/main/java/TestClass.java
@@ -0,0 +1,6 @@
+public class TestClass {
+    public static void main(String[] args) {
+        String s = "com.carbonhealth.folder1.Greeter";
+        System.out.println(s.substring(0,s.lastIndexOf('.')));
+    }
+}
